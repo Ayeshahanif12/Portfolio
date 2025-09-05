b9c4ee7e32de946dc2f5807f20092d8bcc3df511
Ayeshahanif12 <ayeshahanif.0317@gmail.com>
2025-09-05 15:25:40 +0500

    dd

--- script.js
+++ script.js
@@ -32,11 +32,4 @@ AOS.init({
   duration: 1000,
   once: true
 });
-// JS for navbar toggle
-const toggle = document.querySelector(".menu-toggle");
-const menu = document.querySelector("nav ul");
-
-toggle.addEventListener("click", () => {
-  menu.classList.toggle("show");
-})
 
